8438c24d076bff1b271a6017075fdcd3b1ecc1e9
Chyld Medford <chyld.medford@gmail.com>
2017-09-20 05:41:43 +0000

    wip

--- main.go
+++ main.go
@@ -19,7 +19,15 @@ type login struct {
 }
 
 func main() {
-	var x int
-	db, err := sql.Open("postgres", "postgres://postgres:pass1234@localhost/temp")
+	db, err := sql.Open("postgres", "postgres://postgres:pass1234@localhost/temp?sslmode=disable")
 	fmt.Printf("db: %#v, err: %#v", db, err)
+	rows, err := db.Query("SELECT * FROM logins")
+	fmt.Printf("rows: %#v, err: %#v", rows, err)
+	for rows.Next() {
+		l := login{}
+		err := rows.Scan(&l.ID, &l.Email, &l.Username, &l.Password, &l.Pin, &l.Site, &l.Meta, &l.CreatedAt)
+		fmt.Printf("\n\n\nerr: %#v, login: %#v", err, l)
+		y, m, d := l.CreatedAt.Date()
+		fmt.Printf("\n\n\ny: %#v, m: %#v, d: %#v", y, m, d)
+	}
 }
